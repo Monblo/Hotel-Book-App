6bdad42cfbe858442401b0017cc8e481d5f84c3f
Monblo <bloniarz@live.com>
2021-07-13 20:31:48 +0200

    Add room data  Context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.scss';
 import {Home} from "./views/Home/Home";
 import {
@@ -10,10 +10,34 @@ import {
 } from 'react-router-dom';
 import {Booking} from "./views/Booking/Booking";
 import {NotFound} from "./views/NotFound/NotFound";
-
+import {RoomContext} from "./Layout/Context/RoomContext";
+import data from "./data";
 
 const App = () => {
+    const [rooms, setRooms] = useState([]);
+    const type = 'all';
+    const capacity = 1;
+    const [price, setPrice] = useState(0);
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(0);
+
+    const formatData = (items) => {
+        return items.map(item => {
+            let id = item.sys.id;
+            return {...item.fields, id}
+        })
+    };
+
+    useEffect( () => {
+        setRooms(formatData(data))
+    },[]);
+
+    console.log(rooms)
+
+    const roomContextValue = {rooms, type, capacity, price, minPrice, maxPrice}
+
   return (
+      <RoomContext.Provider value={roomContextValue}>
    <HashRouter>
        <>
        <Switch>
@@ -23,6 +47,7 @@ const App = () => {
        </Switch>
        </>
    </HashRouter>
+      </RoomContext.Provider>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        sys: {
+            id: '1'
+        },
+        fields: {
+            name: 'standard room',
+            type: 'standard',
+            price: 50,
+            capacity: 2,
+            qty: 10
+        }
+    },
+    {
+        sys: {
+            id: '2'
+        },
+        fields: {
+            name: 'suite deluxe',
+            type: 'deluxe',
+            price: 100,
+            capacity: 3,
+            qty: 4
+        }
+    }
+]
